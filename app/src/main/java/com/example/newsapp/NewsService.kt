578ba257bcfa28a9d918d50f19d83e466a59d637
Abdullah Abbasi <abdullahabbasi852@gmail.com>
2020-09-27 14:44:46 +0500

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/NewsService.kt
@@ -0,0 +1,26 @@
+package com.example.newsapp
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val BASE_URL = "https://newsapi.org/"
+const val API_KEY = "16384b64771b4db099872af646ddd329"
+interface NewsInterface{
+    @GET("v2/top-headlines?apiKey=$API_KEY")
+    fun getHeadlines(@Query("country") country: String, @Query("page") page: Int) : Call<News>
+
+}
+
+object NewsService{
+    val newsInstance: NewsInterface
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        newsInstance = retrofit.create(NewsInterface::class.java)
+    }
+}
